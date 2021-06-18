b8676bbb98dd5203e50588139510743a0cf0ce24
Exusiai <2246834404@qq.com>
2021-06-18 20:04:14 +0800

    1

--- src/create_pdf.java
+++ src/create_pdf.java
@@ -1,9 +1,18 @@
-
+import java.io.File;
 import java.io.IOException;
-public class create_pdf{
-    public static void main(String fileName) throws IOException{
-        PDDocument document = new PDDocument();
-        document.save(String.format('/pdfs/%s', fileName));
-        document.close();
+import org.apache.pdfbox.pdmodel.PDDocument;
+import java.nio.file.Files;
+
+public class create_pdf {
+    public static void create(String fileName) throws IOException {
+        PDDocument doc = new PDDocument();
+        File directory = new File("/pdfs/");
+        System.out.println(directory.exists());
+        if (directory.exists() == false)
+            directory.mkdir();
+        String path = String.format("pdfs/%s.pdf", fileName);
+        System.out.println(path);
+        doc.save(path);
+        doc.close();
     }
 }
