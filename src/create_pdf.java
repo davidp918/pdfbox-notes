2074b86d276535ae57ea6433186f04ba033c2d15
Exusiai <2246834404@qq.com>
2021-06-18 16:33:54 +0800

    Update create_pdf.java

--- src/create_pdf.java
+++ src/create_pdf.java
@@ -1,3 +1,9 @@
-public static void main(String[] args) {
-    
+
+import java.io.IOException;
+public class create_pdf{
+    public static void main(String fileName) throws IOException{
+        PDDocument document = new PDDocument();
+        document.save(String.format('/pdfs/%s', fileName));
+        document.close();
+    }
 }
