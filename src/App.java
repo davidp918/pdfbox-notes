67644c6b4c16f71609bd7e200c522dcd88ea384d
Exusiai <2246834404@qq.com>
2021-06-22 13:45:18 +0800

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,18 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String fileName = "tet";
+        String fileName = "document";
         Init.create(fileName, 4);
+
         /*
-         * AddText.addOneLineText(fileName, 0, "Sample one line text", new Float[] {
-         * 20.0f, 100.0f }); AddText.addMultipleLineText(fileName, 1,
+         * * Text.addOneLine(fileName, 0, "Sample one line text", new Float[] { 20.0f,
+         * 100.0f }); Text.addMultipleLine(fileName, 1,
          * "Sample multiple lines texts texts sample", new Float[] { 100.0f, 700.0f });
-         * 
-         * remove.removePage(fileName, 2); System.out.println(Extract.readText("CAS"));
-         * System.out.println(Extract.passport("CAS"));
+         * Page.remove(fileName, 2); System.out.println(Text.extractPassport("CAS"));
+         * System.out.println(Text.extractText("CAS"));
          * System.out.println(MetaData.set(fileName));
-         * System.out.println(MetaData.get(fileName));
+         * System.out.println(MetaData.get(fileName)); Attachment.add(fileName);
+         * System.out.println(Attachment.get(fileName));
          */
-        Attachment.add(fileName);
-        System.out.println(Attachment.get(fileName));
     }
 }
